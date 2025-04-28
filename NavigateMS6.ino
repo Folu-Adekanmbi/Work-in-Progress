3778b27c743e46179edc2f8da27e07f8369d6af1
AnayK11 <139814477+AnayK11@users.noreply.github.com>
2025-04-28 11:48:55 -0400

    Update NavigateMS6.ino

--- NavigateMS6.ino
+++ NavigateMS6.ino
@@ -213,7 +213,7 @@ void setup() {
   pinMode(11, OUTPUT); pinMode(13, OUTPUT); pinMode(12, OUTPUT);
   pinMode(10, OUTPUT); pinMode(9, OUTPUT); pinMode(8, OUTPUT);
     pinMode(2, OUTPUT); pinMode(3, OUTPUT); pinMode(4, OUTPUT);
-        pinMode(5, OUTPUT); pinMode(6, OUTPUT); pinMode(7, OUTPUT);
+        pinMode(5, OUTPUT); pinMode(6, OUTPUT); pinMode(7, OUTPUT); pinMode(S, OUTPUT);
 
 
   //pinMode(BL_IN1, OUTPUT); pinMode(BL_IN2, OUTPUT); pinMode(BL_EN, OUTPUT);
